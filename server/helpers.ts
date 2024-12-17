58db5b4e9d785f9d801850475a396b8aa2c9e748
Logan Sparlin <logan.sparlin@gmail.com>
2024-12-17 14:58:36 -0500

    import node:crypto vs crypto

--- server/helpers.ts
+++ server/helpers.ts
@@ -1,4 +1,8 @@
-import crypto from 'crypto';
+import {
+  createCipheriv,
+  createDecipheriv,
+  randomBytes,
+} from 'node:crypto';
 
 const key = useRuntimeConfig().encryptionKey;
 
@@ -8,8 +12,8 @@ export const encryptPassword = (plaintext: string) => {
   }
 
   const keyBuffer = Buffer.from(key, 'hex');
-  const iv = crypto.randomBytes(16);
-  const cipher = crypto.createCipheriv('aes-256-cbc', keyBuffer, iv);
+  const iv = randomBytes(16);
+  const cipher = createCipheriv('aes-256-cbc', keyBuffer, iv);
   let encrypted = cipher.update(plaintext, 'utf8', 'hex');
   encrypted += cipher.final('hex');
   return `${iv.toString('hex')}:${encrypted}`;
@@ -24,7 +28,7 @@ export const decryptPassword = (ciphertext: string) => {
   const keyBuffer = Buffer.from(key, 'hex');
   const [ivHex, encrypted] = ciphertext.split(':');
   const iv = Buffer.from(ivHex, 'hex');
-  const decipher = crypto.createDecipheriv('aes-256-cbc', keyBuffer, iv);
+  const decipher = createDecipheriv('aes-256-cbc', keyBuffer, iv);
   let decrypted = decipher.update(encrypted, 'hex', 'utf8');
   decrypted += decipher.final('utf8');
   return decrypted;
