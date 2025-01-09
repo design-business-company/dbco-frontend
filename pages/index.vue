39779d8b419e7eef0342375ddbefc7bd9f872117
Jordan Egstad <jordanegstad@gmail.com>
2025-01-09 10:41:12 -0600

    test: useSeoMeta

--- pages/index.vue
+++ pages/index.vue
@@ -55,7 +55,16 @@ onMounted(() => {
  * Handle SEO Shit
  * --------------------------------------------------------------------------*/
 
-useSeoMeta(pageSEO(data.value?.seo));
+// useSeoMeta(pageSEO(data.value?.seo));
+
+useSeoMeta({
+  title: "My Amazing Site",
+  ogTitle: "My Amazing Site",
+  description: "This is my amazing site, let me tell you all about it.",
+  ogDescription: "This is my amazing site, let me tell you all about it.",
+  ogImage: "https://example.com/image.png",
+  twitterCard: "summary_large_image",
+});
 
 // PageSetup({
 //   seoMeta: {
