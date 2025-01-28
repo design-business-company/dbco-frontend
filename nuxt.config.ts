ba6dfc8b6d07a9cebf57a4dd1057ebcc8ffe0216
Jordan Egstad <jordanegstad@gmail.com>
2025-01-27 18:33:29 -0600

    fix: errors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -118,7 +118,5 @@ export default defineNuxtConfig({
     },
   },
 
-  // this has been causing a netlify error:
-  // error decoding lambda response: error decoding lambda
   compatibilityDate: "2025-01-27",
 });
